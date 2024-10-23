43be0248e3a3a04c3e4016103820618ee3ec5109
AlinaGeiger <alina.geiger@sbw-media.ch>
2024-10-23 14:03:09 +0200

    joki

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,12 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            studentId: 0,
             dashboardData: [],
         }
     },
+    methods: {
+        async loadDashboard() {
+            const response = await fetch('https://api-sbw-plc.sbw.media/Timesheetview');
+            const data = await response.json();
+            this.dashboardData = data.resources;
+        },
+    },
     mounted() {
         this.studentId = JSON.parse(localStorage.getItem('selectedStudent'));
+        this.loadDashboard();
         
     },
+
 }).mount('#app');
